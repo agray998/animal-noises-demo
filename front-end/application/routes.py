d34ff61970949949b74a7098a3b94ca1b03bd22c
agray998 <a.gray.4797@gmail.com>
2022-03-23 15:34:53 +0000

    added index.html to front-end

--- front-end/application/routes.py
+++ front-end/application/routes.py
@@ -1,8 +1,9 @@
 from application import app
+from flask import render_template
 import requests
 
 @app.route('/')
 def index():
     animal = requests.get('http://animal-api:5000/get-animal')
     noise = requests.post('http://noise-api:5000/noise', json=animal.json())
-    return f'{animal.json()["animal"]} goes {noise.json()["noise"]}'
+    return render_template('index.html', animal_noise=f'{animal.json()["animal"]} goes {noise.json()["noise"]}')
